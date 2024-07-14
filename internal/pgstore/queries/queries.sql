1e00da20d2f7cc6513102a3e95cd701ed7f9c017
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-07-14 01:51:20 -0300

    refactor: update queries.

--- internal/pgstore/queries/queries.sql
+++ internal/pgstore/queries/queries.sql
@@ -1,19 +1,20 @@
--- name: InsetTrip :one
-INSERT INTO trips
-  ( "destination", "owner_email", "owner_name", "starts_at", "ends_at" ) VALUES
+-- name: InsertTrip :one
+INSERT
+INTO trips
+  ( "destination", "owner_email", "owner_name", "starts_at", "ends_at") VALUES
   ( $1, $2, $3, $4, $5 )
 RETURNING "id";
 
 -- name: GetTrip :one
-SELECT 
-  "destination", "owner_email", "owner_name", "starts_at", "ends_at"
+SELECT
+  "id", "destination", "owner_email", "owner_name", "is_confirmed", "starts_at", "ends_at"
 FROM trips
-WHERE 
+WHERE
   id = $1;
 
 -- name: UpdateTrip :exec
 UPDATE trips
-SET
+SET 
   "destination" = $1,
   "ends_at" = $2,
   "starts_at" = $3,
